2bbc54dca75bbfe1428aa65a88b3d16cfed0a3bd
Matt Girdler <matt.girdler@digital.landregistry.gov.uk>
2017-12-15 11:39:22 +0000

    Change https to http for bluemix links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  instantMortgageAPI: 'https://hmlr-ds-instantmortgageapi.eu-gb.mybluemix.net', // :3000
-  instantMortgageUI: 'https://hmlr-ds-instantmortgageui.eu-gb.mybluemix.net', // :8100
+  instantMortgageAPI: 'http://hmlr-ds-instantmortgageapi.eu-gb.mybluemix.net', // :3000
+  instantMortgageUI: 'http://hmlr-ds-instantmortgageui.eu-gb.mybluemix.net', // :8100
   landingScreen: 'http://hmlr-ds-landingscreen.eu-gb.mybluemix.net', // :4200
-  paulPropertyAdvisorUI: 'https://hmlr-ds-paul-property-advisor-ui.eu-gb.mybluemix.net', // :3000
-  personAPI: 'https://hmlr-ds-personapi.eu-gb.mybluemix.net', // :9002
-  propertyAPI: 'https://hmlr-ds-propertyapi.eu-gb.mybluemix.net', // :9001
-  transactionAPI: 'https://hmlr-ds-transactionapi.eu-gb.mybluemix.net', // :6001
-  transactionUI: 'https://hmlr-ds-transactionui.eu-gb.mybluemix.net', // :8081
+  paulPropertyAdvisorUI: 'http://hmlr-ds-paul-property-advisor-ui.eu-gb.mybluemix.net', // :3000
+  personAPI: 'http://hmlr-ds-personapi.eu-gb.mybluemix.net', // :9002
+  propertyAPI: 'http://hmlr-ds-propertyapi.eu-gb.mybluemix.net', // :9001
+  transactionAPI: 'http://hmlr-ds-transactionapi.eu-gb.mybluemix.net', // :6001
+  transactionUI: 'http://hmlr-ds-transactionui.eu-gb.mybluemix.net', // :8081
   composerExplorer: 'http://169.51.23.118:31090' // :3000
 };
